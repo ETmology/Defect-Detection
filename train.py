a2ebf37338f52b1e03a1800c31e80be58b2a20be
ETmology <etmooooology@gmail.com>
2024-05-30 23:56:13 +0800

    +模型还需要优化

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     # 指定训练数据集的路径和其他参数
     data = 'NEU-DET.yaml'  # 数据集的yaml文件
     epochs = 200  # 训练轮数
-    imgsz = 640  # 输入图像尺寸
+    imgsz = 200  # 输入图像尺寸
     device = '0'  # 使用的GPU设备编号
 
     # 开始训练
